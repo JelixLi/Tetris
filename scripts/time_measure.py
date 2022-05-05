0c028d1dd6d066720c4711b7c76bdb3efa90bfb9
JelixLi <1512510237@qq.com>
2022-05-05 06:12:44 -0400

    add cli

--- scripts/time_measure.py
+++ scripts/time_measure.py
@@ -5,6 +5,3 @@ text = sys.stdin.read()
 pattern=re.compile('Took (.+) microseconds') 
 result=pattern.findall(text)
 print("Took " + result[0] + " microseconds")
-
-# $ sudo docker logs c4e527f25017d9b47630cb9b072a5349ae9ee8797f5f99511c690ac4c50de65a | python time_measure.py 
-# Took 96265 microseconds
